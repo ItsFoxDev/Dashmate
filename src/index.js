bfa27fd8e15d3a90e37fe80295a2fec23651fba0
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2024-06-26 15:14:25 -0400

    🔧 Fixed Unsplash API

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ function closeSettings(){
   document.getElementById('settingsWindow').classList.remove('show');
   cl('🔧 Closed Settings menu')
 }
-settingsMenu();
+
 function switchSetting(e){
   switchClass(e,'active');
   switchClass(document.querySelector('.menu[menu="'+e.textContent.trim()+'"]'),'activeMenu');
@@ -93,6 +93,10 @@ function updateSettings(){
     e.innerHTML = settings[e.getAttribute('s')];
     cl('🔁 Replaced '+e.getAttribute('s')+' with '+settings[e.getAttribute('s')]);
   })
+  document.querySelectorAll('input[setting]').forEach(function(e){
+    e.value = settings[e.getAttribute('setting')];
+    cl('🔁 Replaced '+e.getAttribute('s')+' with '+settings[e.getAttribute('s')]);
+  })
   // Update the theme colour
   // document.getElementById('themecolour').innerHTML=':root{--th:'+settings.theme+' !important;}';
   // document.getElementById('themefont').innerHTML='body{--f:'+settings.font+';--font:var(--f, JetBrains Mono) !important;}';
@@ -102,6 +106,18 @@ function updateSettings(){
   toggleClassesArray.forEach(function(e){
     document.body.classList.toggle(e, settings[e]);
   });
+  if (settings.darkmode){
+    document.body.classList.add('darkmode')
+  } else {
+    document.body.classList.remove('darkmode')
+  }
+  if (settings.legacyapi){
+    document.getElementById('newapi').style.display='none';
+    document.getElementById('legacyapi').style.display='block';
+  } else {
+    document.getElementById('newapi').style.display='block';
+    document.getElementById('legacyapi').style.display='none';
+  }
   document.getElementById('bgimg').src=settings.backgroundid;
   // document.querySelector('header > i').classList='fas fa-'+settings.windowicon;
   // document.querySelector('#iconpreview').classList='fas fa-'+settings.windowicon;
@@ -165,32 +181,34 @@ function refreshWeather(){
 
 /* =====[ WALLPAPER FEED ]==================== */
 function loadMainFeed(){
-  loadUsImages('nature','naturefeed',24);
+  loadUsImages('landscape','naturefeed');
+  loadUsImages('city','cityfeed');
 }
 var co;
-function loadUsImages(c,e,n){
+
+async function loadUsImages(c,e,n){
   co=c;
-  for (let step = 0; step < n; step++) {
-    var img = document.createElement('img');
-    img.classList.add('unloaded');
-    document.getElementById(e).appendChild(img);
-    img.onclick=function(){
-      sw(this)
-    }
-    setTimeout(function(){
-      if (co === c){
-        var el = document.querySelector('#'+e+' img:not([src])');
-        el.src = 'https://source.unsplash.com/random?'+c+'&cheesewick='+Math.random();
-        el.onload = function(){
-          el.classList.remove('unloaded')
-        };
-      }
-      if (step === 23 && co === 'nature'){
-        loadUsImages('city','cityfeed',24);
-      }
-    }, step*120);
-  }
+  fetch('./wallpapers/'+c+'.json')
+  .then(response => {
+    if (!response.ok) {throw new Error('Error: ' + response.statusText);}
+    return response.json();
+  })
+  .then(data => {
+    data.forEach(item => {
+      const rawUrl = item.urls.small;
+      console.log('Added item ' + rawUrl);
+      const img = document.createElement('img');
+      img.src = rawUrl;
+      img.addEventListener('click', function() {
+        sw(item.urls.full);
+      });
+      img.alt = item.alt_description || 'Image';
+      document.getElementById(e).appendChild(img);
+    });
+  })
+  .catch(error => console.error('Error fetching the JSON data:', error));
 }
+
 loadMainFeed();
 var cc;
 
@@ -205,9 +223,194 @@ function wc(l,c){
 }
 
 function sw(e){
-  document.getElementById('bgimg').src=e.src;
-  settings.backgroundid=e.src;
+  // const url = new URL(e.src);
+  // url.search = '';
+  // const cleanSrc = url.toString();
+
+  document.getElementById('bgimg').src=e;
+  settings.backgroundid=e;
   window.store.set('settings',JSON.stringify(settings));
   updateSettings();
-  cl('🖼️ Set background to '+e.src);
+  cl('🖼️ Set background to '+e);
 }
+
+function qws(){
+  
+  const options = {method: 'GET', headers: {'User-Agent': 'insomnia/8.6.1'}};
+
+  fetch('https://geocoding-api.open-meteo.com/v1/search?name='+document.getElementById('weathersearch').value+'&count=10&language=en&format=json', options)
+    .then(response => response.json())
+    .then(data => pws(data))
+    .catch(err => console.error(err)); 
+}
+function pws(jr){
+  document.getElementById('weathersearchresults').innerHTML=null;
+  jr.results.forEach(function(e){
+    var el = document.createElement('button');
+    el.innerHTML='<img class="wqi" src="https://flagsapi.com/'+e.country_code+'/flat/64.png"> '+e.name+', '+e.admin1+', '+e.country;
+    el.onclick=function(){
+      settings.weatherlat=e.latitude;
+      settings.weatherlong=e.longitude;
+      updateSettings();
+    }
+    document.getElementById('weathersearchresults').appendChil
+    d(el);
+  })
+}
+
+/* =====[ TIME DISPLAY ]====================== */
+setInterval(function(){
+  const date = new Date();
+  const h12 = date.getHours() % 12 || 12;
+  const h24 = date.getHours();
+  const min = date.getMinutes().toString().padStart(2, '0');
+  const sec = date.getSeconds().toString().padStart(2, '0');
+  var hrs = h12;
+  if (settings.clock24){
+    var hrs = h24;
+  }
+  var ts='<ts>:</ts>';
+  if (settings.flashsep){
+    var ts='<ts class="flash">:</ts>';
+  }
+  var sd=sec;
+  // if (settings.showseconds){
+  //   var sd = ts+sec
+  // }
+  var ap='';
+  if (settings.showampm){
+    var ap = ' '+date.getHours() >= 12 ? 'PM' : 'AM';
+  }
+  // document.getElementById('time').innerHTML=hrs+ts+min+sd+' '+ap;
+  document.querySelectorAll('tih').forEach(function(e){e.innerHTML=hrs});
+  document.querySelectorAll('tim').forEach(function(e){e.innerHTML=min});
+  document.querySelectorAll('tia').forEach(function(e){e.innerHTML=ap});
+  if (settings.showseconds){
+    document.querySelectorAll('tis').forEach(function(e){e.innerHTML=sec});
+    document.querySelectorAll('#secsep').forEach(function(e){e.innerHTML=': '});
+  } else {
+    document.querySelectorAll('tis').forEach(function(e){e.innerHTML=null});
+    document.querySelectorAll('#secsep').forEach(function(e){e.innerHTML=' '});
+  }
+  if (settings.flashsep){
+    if (settings.smoothflash){
+      document.querySelectorAll('ts').forEach(function (e){e.classList.add('flashs')});
+      document.querySelectorAll('ts').forEach(function (e){e.classList.remove('flash')});
+    } else {
+      document.querySelectorAll('ts').forEach(function (e){e.classList.add('flash')});
+      document.querySelectorAll('ts').forEach(function (e){e.classList.remove('flashs')});
+    }
+  } else {
+    document.querySelectorAll('ts').forEach(function (e){e.classList.remove('flash')});
+    document.querySelectorAll('ts').forEach(function (e){e.classList.remove('flashs')});
+  }
+}, 100);
+var weatherapi = '8447eb979b514698b6625906242803';
+
+
+function fetchWeather(){
+  console.log('🌡️ Fetching Weather');
+  var fw = document.getElementById('fw');
+  fw.onClick=null;
+  fw.innerHTML='<i class="fas fa-loader"></i>';
+  if (settings.legacyapi){
+    var tunit = '';
+    if (!settings.usecelsius){
+      var tunit='&temperature_unit=fahrenheit'
+    }
+    cl('🌡️ Fetching weather')
+    const options = {method: 'GET', headers: {'User-Agent': 'dashmate'}};
+    fetch('https://api.open-meteo.com/v1/forecast?latitude='+settings.weatherlat+'&longitude='+settings.weatherlong+'&current=temperature_2m,apparent_temperature,is_day,precipitation,weather_code'+tunit, options)
+      .then(response => response.json())
+      .then(response => weatherParse(response))
+      .catch(err => console.error(err));
+  } else {
+    const options = {method: 'GET', headers: {'User-Agent': 'insomnia/8.6.1'}};
+
+    fetch('https://api.weatherapi.com/v1/current.json?key='+weatherapi+'&q='+settings.weatherloc+'&aqi=no', options)
+      .then(response => response.json())
+      .then(response => weatherParse(response))
+      .catch(err => console.error(err));
+  }
+}
+var wbt;
+function weatherParse(r){
+  if (settings.legacyapi){
+    var ts = 'temperature_2m';
+    if (settings.usefeelslike){
+      var ts = 'apparent_temperature';
+    }
+    var wc = r.current.weather_code.toString().padStart(2, '0');
+    if (wc === '00') {wc = '01';}
+    console.log(r);
+    console.log(r.current[ts]);
+    console.log(r.current_units[ts])
+    document.getElementById('weathertemp').innerHTML=r.current[ts]+r.current_units[ts];
+    document.getElementById('weatherprecip').innerHTML=r.current.precipitation+r.current_units.precipitation;
+    var dn = r.current.is_day ? "d" : "n";
+    document.getElementById('weatherimg').src = `https://openweathermap.org/img/wn/${wc}${dn}@2x.png`;
+  } else {
+    var tempd;
+    if (settings.usecelsius){
+      tempd=r.current.temp_c+'°C';
+    } else {
+      tempd=r.current.temp_f+'°F';
+    }
+    document.getElementById('weathertemp').innerHTML = tempd;
+    document.getElementById('weatherprevtemp').innerHTML = tempd;
+    document.getElementById('weathersub').innerHTML=r.current.condition.text;
+    document.getElementById('weatherprevdesc').innerHTML=r.current.condition.text;
+    document.getElementById('weatherimg').src='https:'+r.current.condition.icon;
+    document.getElementById('weatherprevimg').src='https:'+r.current.condition.icon;
+    document.getElementById('weatherlocation').innerHTML=r.location.name+', '+r.location.region+', '+r.location.country;
+  }
+  var fw = document.getElementById('fw');
+  fw.innerHTML='<i class="fa-solid fa-hourglass-clock"></i>';
+  clearTimeout(wbt);
+  wbt = setTimeout(function(){
+    fw.innerHTML='<i class="fas fa-rotate-right"></i>';
+    fw.onClick='fetchWeather();';
+  },5E3)
+}
+
+var shuffleclock;
+function autoShuffle(cat){
+  settings.shufflecat=cat;
+  window.store.set('settings',JSON.stringify(settings));
+  
+}
+
+var shuffleclock = setInterval(function(){
+  if (settings.autoshuffle){
+    document.getElementById('bgimg').src = data.urls.regular;
+  }
+},1000)
+
+function uas(){
+  clearInterval(shuffleclock);
+  setTimeout(function(){
+    var shuffleclock = setInterval(function(){
+      // document.getElementById('bgimg').src='https://source.unsplash.com/random?'+settings.shufflecat+'&anticache='+Math.random();
+      fetch('https://api.unsplash.com/photos/random?query=' + settings.shufflecat + '&client_id=JWvq8s9WqJDTgx7nBCX23mvUxTVyOIYVS-oeKDA7Bug')
+        .then(response => response.json())
+        .then(data => {
+          document.getElementById('bgimg').src = data.urls.regular;
+        })
+        .catch(err => console.error(err));
+    }, settings.shuffleinterval * 60000);
+  },100);
+}
+
+
+
+
+
+
+// Run after a bit
+
+setTimeout(function(){
+  fetchWeather()
+}, 5E2);
+
+setInterval(function(){fetchWeather()}, 6E4);
+fetchWeather();
--- src/main.js
+++ src/main.js
@@ -1,63 +1,119 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+// Modules to control application life and create native browser window
+const { screen, app, BrowserWindow, ipcMain } = require('electron');
 const path = require('node:path');
 
 const Store = require('electron-store');
 const store = new Store();
 
+let splash; // Declare the splash screen variable
+let mainWindow; // Declare the main window variable
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
-
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    minWidth:800,
-    height: 600,
-    minHeight:600,
-    titleBarStyle: 'hiddenInset',
-    trafficLightPosition: { x: 10, y: 10 },
-    title: "Dashmate",
+function rc(o,w,h){
+  const { width: screenWidth, height: screenHeight } = screen.getPrimaryDisplay().workAreaSize
+  const x = Math.round((screenWidth - w) / 2)
+  const y = Math.round((screenHeight - h) / 2)
+  o.setBounds({x, y, width: w, height: h }, true)
+}
+function createWindow () {
+  // Create the splash screen. This will show before the main window is shown
+  splash = new BrowserWindow({
+    width: 400,
+    height: 300,
+    resizable: false,
+    frame: false,
     webPreferences: {
-      preload: path.resolve(__dirname, 'preload.js'),
-      nodeIntegration:true
-    }
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    },
+    backgroundColor: '#1B1A1D' // Set the background color to 1B1A1D
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  // setTimeout(() => {
+  //   rc(splash,  800, 600)
+  // }, 5000);
+  // Load the splash screen html
+  splash.loadFile('./src/splash/splash.html');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+  // After 5 seconds, close the splash screen and open the main window
+  splash.webContents.on('did-finish-load', () => {
+    setTimeout(() => {
+      // Check if the splash screen still exists before trying to close it
+      if (!splash.isDestroyed()) {
+        splash.close();
+      }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
+      // Create the main window after the splash screen is closed
+      mainWindow = new BrowserWindow({
+        width: 800,
+        minWidth:800,
+        height: 600,
+        minHeight:600,
+        titleBarStyle: 'hiddenInset',
+        trafficLightPosition: { x: 10, y: 10 },
+        title: "Dashmate",
+        webPreferences: {
+          preload: path.resolve(__dirname, 'preload.js'),
+          nodeIntegration:true
+        },
+        backgroundColor: '#1B1A1D'
+      });
+      mainWindow.setMenuBarVisibility(false)
 
-  // On OS X it's common to re-create a window in the app when the
+      // Load the index.html of the app.
+      mainWindow.loadFile('./src/index.html');
+
+      // Open the DevTools.
+      // mainWindow.webContents.openDevTools();
+
+      mainWindow.on('enter-full-screen', () => {
+        mainWindow.setWindowButtonVisibility(true);
+      });
+      
+      mainWindow.on('leave-full-screen', () => {
+        mainWindow.setWindowButtonVisibility(true);
+      });
+    }, 5e3);
+  });
+}
+
+app.on('ready', createWindow);
+
+process.on('uncaughtException', error => {
+	console.error('Exception:', error); 
+	app.exit(1);
+});
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
+  if (BrowserWindow.getAllWindows().length === 0 && !mainWindow) createWindow()
+})
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+app.on('window-all-closed', function () {
+  app.quit()
+})
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+
+/* 
+
+store.set('unicorn', '🦄');
+console.log(store.get('unicorn'));
+//=> '🦄'
+
+Use dot-notation to access nested properties
+store.set('foo.bar', true);
+console.log(store.get('foo'));
+//=> {bar: true}
+
+store.delete('unicorn');
+console.log(store.get('unicorn'));
+//=> undefined 
+
+*/
 
 
 ipcMain.handle('get', (event, key) => {
@@ -67,3 +123,97 @@ ipcMain.handle('get', (event, key) => {
 ipcMain.on('set', (event, key, value) => {
   store.set(key, value);
 });
+
+// const { app, BrowserWindow, ipcMain } = require('electron');
+// const path = require('node:path');
+
+// const Store = require('electron-store');
+// const store = new Store();
+
+// // Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// if (require('electron-squirrel-startup')) {
+//   app.quit();
+// }
+
+// const createWindow = () => {
+//   // Create the browser window.
+//   const mainWindow = new BrowserWindow({
+//     width: 800,
+//     minWidth:800,
+//     height: 600,
+//     minHeight:600,
+//     titleBarStyle: 'hiddenInset',
+//     trafficLightPosition: { x: 10, y: 10 },
+//     title: "Dashmate",
+//     webPreferences: {
+//       preload: path.resolve(__dirname, 'preload.js'),
+//       nodeIntegration:true
+//     }
+//   });
+
+//   // and load the index.html of the app.
+//   mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+//   // Open the DevTools.
+//   mainWindow.webContents.openDevTools();
+// };
+// const createSplash = () => {
+//   // Create the browser window.
+//   const splashWindow = new BrowserWindow({
+//     width: 400,
+//     height: 300,
+//     resizable: false,
+//     frame: false,
+//     trafficLightPosition: { x: 10, y: 10 },
+//     title: "Dashmate",
+//     webPreferences: {
+//       preload: path.resolve(__dirname, 'preload.js'),
+//       nodeIntegration:true
+//     }
+//   });
+
+//   // and load the index.html of the app.
+//   splashWindow.loadFile(path.join(__dirname, 'index.html'));
+
+//   // Open the DevTools.
+//   splashWindow.webContents.openDevTools();
+// };
+
+// // This method will be called when Electron has finished
+// // initialization and is ready to create browser windows.
+// // Some APIs can only be used after this event occurs.
+// app.whenReady().then(() => {
+//   createSplash();
+//   setTimeout(
+//     createWindow(),5e3
+//   );
+
+//   // On OS X it's common to re-create a window in the app when the
+//   // dock icon is clicked and there are no other windows open.
+//   // app.on('activate', () => {
+//   //   if (BrowserWindow.getAllWindows().length === 0) {
+//   //     createWindow();
+//   //   }
+//   // });
+// });
+
+// // Quit when all windows are closed, except on macOS. There, it's common
+// // for applications and their menu bar to stay active until the user quits
+// // explicitly with Cmd + Q.
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit();
+//   }
+// });
+
+// // In this file you can include the rest of your app's specific main process
+// // code. You can also put them in separate files and import them here.
+
+
+// ipcMain.handle('get', (event, key) => {
+//   return store.get(key);
+// });
+
+// ipcMain.on('set', (event, key, value) => {
+//   store.set(key, value);
+// });``
